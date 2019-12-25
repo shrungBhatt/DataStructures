ad4dad9d6946540893aba32dad128494bcebc411
Shrung Bhatt <Shrung Bhatt>
2019-12-25 15:57:04 +0530

    Initial Commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Shrung Bhatt on 25/12/19.
+//  Copyright © 2019 Shrung Bhatt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
